e07c55d98e2689ad5ba1695ca56e416f4cdeacd6
Ainur Iskhakov <94246600+i-Nur23@users.noreply.github.com>
2022-11-12 21:11:58 +0300

    Сделал миграцию и обновил бд

---
+++ Backend/Infrastructure/Library.DAL/Migrations/20221112181005_AddedAllTables.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.DAL.Migrations
+{
+    public partial class AddedAllTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Books",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "CountExamplesAmount",
+                table: "Books");
+
+            migrationBuilder.RenameTable(
+                name: "Books",
+                newName: "Book");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "Book",
+                type: "char(13)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Publisher",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Section",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Year",
+                table: "Book",
+                type: "char(4)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Book",
+                table: "Book",
+                column: "ISBN");
+
+            migrationBuilder.CreateTable(
+                name: "Author",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Author", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookInstance",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ISBN = table.Column<string>(type: "char(13)", nullable: false),
+                    RowNumber = table.Column<int>(type: "int", nullable: false),
+                    IsAvailable = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookInstance", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Librarian",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MobilePhone = table.Column<string>(type: "char(11)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Librarian", x => x.Id);
+                    table.CheckConstraint("CK_Librarian_MobilePhone", "MobilePhone LIKE '[8][9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reader",
+                columns: table => new
+                {
+                    LibraryCard = table.Column<string>(type: "char(6)", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BirthYear = table.Column<string>(type: "char(4)", nullable: false),
+                    StudentCard = table.Column<string>(type: "char(6)", nullable: false),
+                    IsHasDebt = table.Column<bool>(type: "bit", nullable: false),
+                    MobilePhone = table.Column<string>(type: "char(11)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reader", x => x.LibraryCard);
+                    table.CheckConstraint("CK_Reader_BirthYear", "BirthYear LIKE '[1-2][0,8-9][0-9][0-9]'");
+                    table.CheckConstraint("CK_Reader_LibraryCard", "LibraryCard LIKE '[1-9][0-9][0-9][0-9][0-9][0-9]'");
+                    table.CheckConstraint("CK_Reader_MobilePhone", "MobilePhone LIKE '[8][9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'");
+                    table.CheckConstraint("CK_Reader_StudentCard", "StudentCard LIKE '[1-9][0-9][0-9][0-9][0-9][0-9]'");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AuthorBook",
+                columns: table => new
+                {
+                    ISBN = table.Column<string>(type: "char(13)", nullable: false),
+                    AuthorID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthorBook", x => new { x.AuthorID, x.ISBN });
+                    table.ForeignKey(
+                        name: "FK_AuthorBook_Author_AuthorID",
+                        column: x => x.AuthorID,
+                        principalTable: "Author",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AuthorBook_Book_ISBN",
+                        column: x => x.ISBN,
+                        principalTable: "Book",
+                        principalColumn: "ISBN",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "History",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    ReaderID = table.Column<string>(type: "char(6)", nullable: false),
+                    LibrarianID = table.Column<int>(type: "int", nullable: false),
+                    IssueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReturnDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_History", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_History_BookInstance_BookId",
+                        column: x => x.BookId,
+                        principalTable: "BookInstance",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_History_Librarian_LibrarianID",
+                        column: x => x.LibrarianID,
+                        principalTable: "Librarian",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_History_Reader_ReaderID",
+                        column: x => x.ReaderID,
+                        principalTable: "Reader",
+                        principalColumn: "LibraryCard",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Book_Year",
+                table: "Book",
+                sql: "Year LIKE '[1-2][0-9][0-9][0-9]'");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorBook_ISBN",
+                table: "AuthorBook",
+                column: "ISBN");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_History_BookId",
+                table: "History",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_History_LibrarianID",
+                table: "History",
+                column: "LibrarianID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_History_ReaderID",
+                table: "History",
+                column: "ReaderID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuthorBook");
+
+            migrationBuilder.DropTable(
+                name: "History");
+
+            migrationBuilder.DropTable(
+                name: "Author");
+
+            migrationBuilder.DropTable(
+                name: "BookInstance");
+
+            migrationBuilder.DropTable(
+                name: "Librarian");
+
+            migrationBuilder.DropTable(
+                name: "Reader");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Book",
+                table: "Book");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Book_Year",
+                table: "Book");
+
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "Book");
+
+            migrationBuilder.DropColumn(
+                name: "Publisher",
+                table: "Book");
+
+            migrationBuilder.DropColumn(
+                name: "Section",
+                table: "Book");
+
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Book");
+
+            migrationBuilder.RenameTable(
+                name: "Book",
+                newName: "Books");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CountExamplesAmount",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Books",
+                table: "Books",
+                column: "Id");
+        }
+    }
+}
