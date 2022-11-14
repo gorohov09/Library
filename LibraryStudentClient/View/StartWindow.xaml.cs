4e1a0d948ebb5cacfd4d22ce62ab48d76ac32eb8
Данил Калеев <KaleevDA@stud.kai.ru>
2022-11-15 02:40:09 +0300

    Добавлены основные классы, начата реализация первичного окна авторизации/регистрации, хоть и топорно на данный момент

---
+++ LibraryStudentClient/View/StartWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using LibraryStudentClient.View.Authorization;
+
+namespace LibraryStudentClient.View
+{
+    /// <summary>
+    /// Логика взаимодействия для StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+
+            InitializeComponent();
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            MainFrame.Content = new View.Authorization.Authorization();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            this.Close();
+            mainWindow.Show();
+        }
+    }
+}
---
+++ LibraryStudentClient/ViewModel/DataManagerAuthorizationVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryStudentClient.ViewModel
+{
+    public class DataManagerAuthorizationVM : INotifyPropertyChanged
+    {
+
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
