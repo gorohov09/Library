0ccbc1cbf2659926b4ebb618c70527be5a1f3d1a
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-11-15 15:09:28 +0300

    Созда контроллер, отвечающий за регистрацию

---
+++ Backend/Presentation/Library.WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Library.Application.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.WebAPI.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Registrate([FromBody]RequestRegistrate request)
+        {
+            return null;
+        }
+    }
+}
