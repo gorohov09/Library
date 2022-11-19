93a51417ee3654c863d0ae72115ae74972781176
Ainur Iskhakov <94246600+i-Nur23@users.noreply.github.com>
2022-11-19 20:16:25 +0300

    Добавил и описал класс BookDetailsVm

---
+++ Backend/Core/Library.Application/Vm/BookDetailsVm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Application.Vm
+{
+    public class BookDetailsVm
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Publisher { get; set; }
+        public string Year { get; set; }
+        public string Section { get; set; }
+        public IEnumerable<AuthorVm> Authors { get; set; } = new List<AuthorVm>();
+        public int Count { get; set; }
+    }
+}
