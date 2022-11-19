1ede0c88587ad217b8799db02d501ca5e9a95c95
Ghost116rus <danilkaleev@mai.ru>
2022-11-19 21:44:20 +0300

    Резервная копия

--- LibraryStudentClient/Model/Book.cs
+++ LibraryStudentClient/Model/Book.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace LibraryStudentClient.Model
 {
@@ -30,6 +32,28 @@ namespace LibraryStudentClient.Model
                     }));
             }
         }
-
+        /*
+                    <ListView ItemsSource = "{Binding Books}" Foreground="#FF042271" FontSize="16" Margin="0,10,0,0"
+                      SelectedItem="{Binding SelectedBook}">
+                <ListView.Background>
+                    <LinearGradientBrush EndPoint = "0.5,1" StartPoint="0.5,0">
+                        <GradientStop Color = "White" Offset="0.006"/>
+                        <GradientStop Color = "Black" Offset="1"/>
+                        <GradientStop Color = "#FF60C0CC" Offset="0.567"/>
+                        <GradientStop Color = "#FF1D4E69" Offset="0.994"/>
+                        <GradientStop Color = "#FFBBD8D8" Offset="0.087"/>
+                    </LinearGradientBrush>
+                </ListView.Background>
+                <ListView.View>
+                    <GridView>
+                        <GridViewColumn Header = "Название книги" DisplayMemberBinding="{Binding Path=Title}"/>
+                        <GridViewColumn Header = "Раздел" DisplayMemberBinding="{Binding Path=Section}"/>
+                        <GridViewColumn Header = "Авторы" DisplayMemberBinding="{Binding Path=Authors}" />
+                        <GridViewColumn Header = "Издатель" DisplayMemberBinding="{Binding Path=Publisher}"/>
+                        <GridViewColumn Header = "Год издания" DisplayMemberBinding="{Binding Path=Year}"/>
+                    </GridView>
+                </ListView.View>
+            </ListView>
+        */
     }
 }
