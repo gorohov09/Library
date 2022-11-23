851a7dce6d6e009e63e2166cee575c0e9307ded7
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-11-23 19:17:30 +0300

    Рефакторинг

--- Backend/Core/Library.Application/Services/OrderService.cs
+++ Backend/Core/Library.Application/Services/OrderService.cs
@@ -24,6 +24,7 @@ namespace Library.Application.Services
             _librarianRepository = librarianRepository;
             _orderRepository = orderRepository;
         }
+
         public async Task<ResponseOrder> CreateOrder(RequestOrder requestOrder)
         {
             if (requestOrder == null)
