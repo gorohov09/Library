22111ba713a4974af69e6e569061304acc1f7136
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-11-23 09:36:15 +0300

    Создание сущности - заявка

---
+++ Backend/Core/Library.Domain/Entities/OrderEntity.cs
@@ -0,0 +1,35 @@
+﻿using Library.Domain.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library.Domain.Entities
+{
+    public class OrderEntity : BaseEntity
+    {
+        [Required]
+        public int BookInsatnceId { get; set; }
+
+        [Required]
+        [ForeignKey("BookId")]
+        public BookInsatnceEntity BookInsatnce { get; set; }
+
+        [Required]
+        public int ReaderId { get; set; }
+
+        [Required]
+        [ForeignKey("ReaderId")]
+        public ReaderEntity Reader { get; set; }
+
+        [Required]
+        public int LibrarianId { get; set; }
+
+        [Required]
+        [ForeignKey("LibrarianId")]
+        public LibrarianEntity Librarian { get; set; }
+
+        [Required]
+        public DateTime CreationDate { get; set; }
+
+        public DateTime? ExecutionDate { get; set; }
+    }
+}
