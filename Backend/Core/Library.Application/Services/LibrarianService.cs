11d6801765d581799844fd244099bc4cf28c2391
Ainur Iskhakov <94246600+i-Nur23@users.noreply.github.com>
2022-11-24 23:56:02 +0300

    Реализовал получение истории читателя при получении заявки

--- Backend/Core/Library.Application/Services/LibrarianService.cs
+++ Backend/Core/Library.Application/Services/LibrarianService.cs
@@ -10,19 +10,28 @@ namespace Library.Application.Services
     {
         private readonly IOrderRepository _orderRepository;
         private readonly IReaderRepository _readerRepository;
+        private readonly IRecordRepository _recordRepository;
         private IMapper _mapper;
 
-        public LibrarianService(IOrderRepository orderRepository, IReaderRepository readerRepository, IMapper mapper)
+        public LibrarianService(IOrderRepository orderRepository, IReaderRepository readerRepository, IRecordRepository recordRepository, IMapper mapper)
         {
             _orderRepository = orderRepository;
             _readerRepository = readerRepository;
+            _recordRepository = recordRepository; 
             _mapper = mapper;
         }
 
         public async Task<IEnumerable<OrderDetailsForLibrarianVm>> GetLibrarianOrders(int librarianId)
         {
             var librariansOrders = await _orderRepository.GetLibrarianOrders(librarianId);
-            return _mapper.Map<IEnumerable<OrderDetailsForLibrarianVm>>(librariansOrders);
+            var ordersVm = _mapper.Map<IEnumerable<OrderDetailsForLibrarianVm>>(librariansOrders);
+            foreach (var order in ordersVm)
+            {
+                var readerHistory = await _recordRepository.GetReadersHistory(order.Reader.LibraryCard);
+                order.Reader.History = _mapper.Map<IEnumerable<RecordDetailsForReaderVm>>(readerHistory);
+            }
+
+            return ordersVm;
         }
     }
 }
