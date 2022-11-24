8e382eb05660edf080209f22bf3c474fb83bf657
Ainur Iskhakov <94246600+i-Nur23@users.noreply.github.com>
2022-11-24 21:03:24 +0300

    Добавил контроллер читателя

---
+++ Backend/Presentation/Library.WebAPI/Controllers/ReaderController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Library.Application.Interfaces;
+
+namespace Library.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReaderController : ControllerBase
+    {
+        private readonly IReaderService _readerService;
+
+        public ReaderController(IReaderService readerService)
+        {
+            _readerService = readerService;
+        }
+
+        [HttpGet("{libraryCard}")]
+        public async Task<IActionResult> GetUserInfo([FromBody] string libraryCard)
+        {
+            return Ok(_readerService.GetReaderInfo(libraryCard));
+        }
+
+        [HttpGet("{libraryCard}/orders")]
+        public async Task<IActionResult> GetUserOrders([FromBody] string libraryCard)
+        {
+            return Ok();
+        }
+    }
+}
