186184d7851c6487b7625ef62103f6fc717d41f5
Ainur Iskhakov <94246600+i-Nur23@users.noreply.github.com>
2022-11-26 20:26:06 +0300

    Добавил класс Converter

---
+++ Backend/Core/Library.Application/Helpers/Converter.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using Library.Application.Interfaces;
+using Library.Domain.Entities;
+
+namespace Library.Application.Helpers;
+
+public class Converter : IConverter
+{
+    public string GetAuthorsInLine(BookEntity book)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (var author in book.Authors)
+        {
+            stringBuilder.AppendFormat("{0}, ",author.FullName);
+        }
+
+        stringBuilder.Length -= 2;
+
+        return stringBuilder.ToString();
+    }
+}
---
+++ Backend/Core/Library.Application/Interfaces/IConverter.cs
@@ -0,0 +1,8 @@
+﻿using Library.Domain.Entities;
+
+namespace Library.Application.Interfaces;
+
+public interface IConverter
+{
+    public string GetAuthorsInLine(BookEntity book);
+}
