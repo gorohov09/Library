27c81821c36ca721d02f6af7eafe31fd0ba8a114
Ghost116rus <danilkaleev@mai.ru>
2022-11-30 16:35:13 +0300

    Проект начат, окно авторизации готово

---
+++ Frontend/LibrarianClient/DTO/ResponseLoginDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarianClient.DTO
+{
+    internal class ResponseLoginDTO
+    {
+        public bool IsSuccess { get; set; }
+
+        public string Error { get; set; }
+
+        public string LibrarianID { get; set; }
+    }
+}
