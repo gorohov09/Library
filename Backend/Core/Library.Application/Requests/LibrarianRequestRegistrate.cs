7efb5cc8a882b9cc4ad3167c951525217a76e11f
Ainur Iskhakov <94246600+i-Nur23@users.noreply.github.com>
2022-12-02 10:16:25 +0300

    Добавил классы запросов на авторизацию и регистрацию библиотекаря

---
+++ Backend/Core/Library.Application/Requests/LibrarianRequestRegistrate.cs
@@ -0,0 +1,16 @@
+﻿namespace Library.Application.Requests;
+
+public class LibrarianRequestRegistrate
+{
+    public string LastName { get; set; }
+
+    public string Name { get; set; }    
+
+    public string Patronymic { get; set; }
+
+    public string MobilePhone { get; set; }
+
+    public string Login { get; set; }
+
+    public string Password { get; set; }
+}
